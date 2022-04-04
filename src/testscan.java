151eb9666fcc131fdbefa02946de0f766a8276ae
lotarete <93207413+lotarete@users.noreply.github.com>
2022-04-04 17:06:59 +0300

    Scanneri klassid

---
+++ src/testscan.java
@@ -0,0 +1,8 @@
+public class testscan {
+    public static void main(String[] args) {
+        InputScan scan = new InputScan();
+        System.out.println("siseta arv");
+        int i= scan.IntScan();
+        System.out.println(i);
+    }
+}
